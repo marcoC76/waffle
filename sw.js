fdbeefc680ad4ff029e1cceb5adc224100ea962f
Marco Antonio <marco.perez.cetis76@gmail.com>
2019-08-21 11:39:52 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,3 @@
-/* const version = "1";
-const cacheName = `waffle-${version}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-}); */
-
 var offlineCacheName = "wholeSiteCachedv3";
 
 var offlinePageCache = "customOfflinePage";
